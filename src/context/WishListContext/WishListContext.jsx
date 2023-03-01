1fb5c4e3a3ba834823a3820e9c09b19e5d1f3803
Miquel <abellagarciamiquel@gmail.com>
2023-03-01 15:00:54 +0100

    implementes localstorage

--- src/context/WishListContext/WishListContext.jsx
+++ src/context/WishListContext/WishListContext.jsx
@@ -1,17 +1,24 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { places } from "../../data/cities";
 export const WishListContext = createContext(null);
 
 export const WishListProvider = ({ children }) => {
-  const [wishList, setWishList] = useState({
-    family: [],
-    friends: [],
-    couple: [],
-    alone: [],
-    discards: [],
-  });
+  const [wishList, setWishList] = useState(
+    JSON.parse(localStorage.getItem("wishList")) || {
+      family: [],
+      friends: [],
+      couple: [],
+      alone: [],
+      discards: [],
+    }
+  );
 
-  const [countries, setCountries] = useState(places);
+  const [countries, setCountries] = useState(JSON.parse(localStorage.getItem("countries")) || places);
+
+  useEffect(() => {
+    localStorage.setItem("wishList", JSON.stringify(wishList));
+    localStorage.setItem("countries", JSON.stringify(countries))
+  }, [wishList]);
 
   return (
     <WishListContext.Provider
