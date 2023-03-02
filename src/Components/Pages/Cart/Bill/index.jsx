6a8d5193bfa6920ef42e3e6cff4086d0267e440f
Miquel <abellagarciamiquel@gmail.com>
2023-03-02 09:40:22 +0100

    bill display inproved

--- src/Components/Pages/Cart/Bill/index.jsx
+++ src/Components/Pages/Cart/Bill/index.jsx
@@ -18,7 +18,7 @@ export const Bill = () => {
     : 0;
 
   const flightPrice = flight.price ? flight.price : 0;
-console.log(hotelPrice, flightPrice)
+  console.log(hotelPrice, flightPrice);
   return (
     <div className="flex flex-col items-center">
       <div className="p-8 text-center">
@@ -33,9 +33,9 @@ console.log(hotelPrice, flightPrice)
           src="https://images.unsplash.com/photo-1522199710521-72d69614c702?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80"
           alt=""
         />
-        <div className="z-10 bg-white/80 grid grid-cols-1 md:grid-cols-3 py-12 w-full m-4 p-6 md:w-3/4 flex-wrap gap-3 place-items-center text-center">
+        <div className="z-10 bg-white/80 flex flex-col md:flex-row gap-8 items-start justify-start py-12 w-auto m-4 px-24  flex-wrap text-center">
           {flight.destination && (
-            <div className="flex flex-col items-start justify-start h-full">
+            <div className="flex flex-col items-start justify-start h-full md:w-auto text-start">
               <Typography text="FLIGHT" type="subtitle" />
               <Typography text={`From: ${flight.origin}`} />
               <Typography text={`To: ${flight.destination}`} />
@@ -44,9 +44,9 @@ console.log(hotelPrice, flightPrice)
               <Typography text={`Passengers: ${flight.passengers}`} />
             </div>
           )}
-          {/* //voy por aqui */}
+
           {hotel.destination && (
-            <div className="flex flex-col items-start justify-start h-full">
+            <div className="flex flex-col items-start justify-start h-full md:w-auto text-start">
               <Typography text="HOTEL" type="subtitle" />
               <Typography text={`${hotel.name}`} />
               <Typography text={`City: ${hotel.destination}`} />
@@ -56,7 +56,7 @@ console.log(hotelPrice, flightPrice)
             </div>
           )}
           {hotel.destination || flight.destination ? (
-            <div className="flex flex-col items-start justify-start h-full">
+            <div className="flex flex-col items-start justify-start h-full md:w-auto text-start">
               <Typography text="BILL" type="subtitle" />
               {flight.destination && (
                 <Typography text={`Flight: ${flightPrice} €`} />
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -3,24 +3,46 @@ import { createContext, useState } from "react";
 export const CartContext = createContext(null);
 
 export const CartProvider = ({ children }) => {
-  const [cart, setCart] = useState({
-    flight: {
-      origin: null,
-      destination: null,
-      price: null,
-      passengers: null,
-      date: null,
-    },
-    accomodation: {
-      city: null,
-      hotel: null,
-      price: null,
-      image: null,
-      people: null,
-      startDate: null,
-      endDate: null,
-    },
-  });
+  const [cart, setCart] = useState(
+    //     {
+    //     flight: {
+    //       origin: null,
+    //       destination: null,
+    //       price: null,
+    //       passengers: null,
+    //       date: null,
+    //     },
+    //     accomodation: {
+    //       city: null,
+    //       hotel: null,
+    //       price: null,
+    //       image: null,
+    //       people: null,
+    //       startDate: null,
+    //       endDate: null,
+    //     },
+    //   }
+    {
+      flight: {
+        origin: "Andorra la Vella",
+        destination: "Algiers",
+        startDate: "2023-02-02",
+        endDate: "2023-02-03",
+        price: 650,
+        passengers: "5",
+        country: "Algeria",
+      },
+      accomodation: {
+        destination: "Algiers",
+        startDate: "2023-01-01",
+        endDate: "2024-02-02",
+        people: 4,
+        price: 23,
+        name: "Wilderness Vanguard",
+        country: "Algeria",
+      },
+    }
+  );
   return (
     <CartContext.Provider
       value={{
