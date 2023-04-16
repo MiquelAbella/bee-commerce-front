4eb4eea9c1f3be8fdf102689507322725f76ccf8
Miquel <abellagarciamiquel@gmail.com>
2023-04-16 15:29:09 +0200

    ready for deploy

--- src/Components/Pages/Cart/Payment/index.jsx
+++ src/Components/Pages/Cart/Payment/index.jsx
@@ -37,7 +37,7 @@ const CheckoutForm = ({
     if (!error) {
       const { id } = paymentMethod;
       setisLoading(true);
-      const res = await fetch("http://https://bee-commerce-back.vercel.app//checkout", {
+      const res = await fetch("http://https://bee-commerce-back.vercel.app/checkout", {
         method: "POST",
         body: JSON.stringify({
           id,
